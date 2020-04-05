125ee1f1dc1d3b26287252e98e0bd1322c2430aa
akshay-misra-backend-products <akshaymishra2488@gmail.com>
2020-04-05 12:45:05 +0530

    layout config pojo configured

--- framework-core-api/src/main/java/com/bss/framework/core/design/model/LayoutConfig.java
+++ framework-core-api/src/main/java/com/bss/framework/core/design/model/LayoutConfig.java
@@ -1,9 +1,98 @@
 package com.bss.framework.core.design.model;
 
+import com.bss.framework.core.schema.model.Attribute;
 import com.bss.framework.core.schema.model.Base;
+import com.bss.framework.core.schema.model.ObjectType;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 /**
  * Created by Rocky on 15-06-2019.
  */
+@Document(collection="layoutConfigs")
 public class LayoutConfig extends Base {
+
+    @DBRef(lazy = true)
+    ObjectType objectType;
+
+    @DBRef(lazy = true)
+    List<Attribute> attributes;
+
+    String loadAPI;
+
+    String addAPI;
+
+    String updateAPI;
+
+    String deleteAPI;
+
+    private int pageSize;
+
+    String layoutType;
+
+    public ObjectType getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(ObjectType objectType) {
+        this.objectType = objectType;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getLoadAPI() {
+        return loadAPI;
+    }
+
+    public void setLoadAPI(String loadAPI) {
+        this.loadAPI = loadAPI;
+    }
+
+    public String getAddAPI() {
+        return addAPI;
+    }
+
+    public void setAddAPI(String addAPI) {
+        this.addAPI = addAPI;
+    }
+
+    public String getUpdateAPI() {
+        return updateAPI;
+    }
+
+    public void setUpdateAPI(String updateAPI) {
+        this.updateAPI = updateAPI;
+    }
+
+    public String getDeleteAPI() {
+        return deleteAPI;
+    }
+
+    public void setDeleteAPI(String deleteAPI) {
+        this.deleteAPI = deleteAPI;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getLayoutType() {
+        return layoutType;
+    }
+
+    public void setLayoutType(String layoutType) {
+        this.layoutType = layoutType;
+    }
 }
