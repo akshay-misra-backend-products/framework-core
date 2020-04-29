478a0cb1c88ad4f81cd20b5b4b1a08583b6f74cc
akshay-misra-backend-products <akshaymishra2488@gmail.com>
2020-04-30 01:15:43 +0530

    code refactor

--- framework-core-api/src/main/java/com/bss/framework/core/schema/service/api/SchemaService.java
+++
@@ -1,8 +0,0 @@
-package com.bss.framework.core.schema.service.api;
-
-import com.bss.framework.core.schema.model.ObjectType;
-
-public interface SchemaService {
-
-    ObjectType createObjectType(ObjectType objectType);
-}
--- framework-core-api/src/main/java/com/bss/framework/core/schema/service/api/SchemaServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.bss.framework.core.schema.service.api;
-
-
-public abstract class SchemaServiceImpl implements SchemaService {
-
-}
