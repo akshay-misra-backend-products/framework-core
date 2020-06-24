502f3406873a2133e288e49396d1ee7104292d5d
akshay-misra-backend-products <akshaymishra2488@gmail.com>
2020-06-25 02:16:00 +0530

    library modules dpendency fix for main app

--- framework-core-ui/src/main/web/angular-src/src/app/app-routing.module.ts
+++ framework-core-ui/src/main/web/angular-src/src/app/app-routing.module.ts
@@ -9,28 +9,27 @@ import {UserProfileComponent} from "./core/framework/user-profile/user-profile.c
 import {CompositePageComponent} from './core/framework/material-custom-components/composite/composite-page/composite-page.component';
 import {HomeComponent} from "./core/framework/home/home.component";
 
-//noinspection TypeScriptValidateTypes
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: UserProfileComponent/*,
-    canActivate: [AuthGuard]*/
+  { path: 'profile', component: UserProfileComponent,
+    canActivate: [AuthGuard]
   },
-  { path: '', component: HomeComponent/*, canActivate: [AuthGuard]*/},
+  { path: '', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'application/navigation/:objectTypeId/:objectId',
     component: CompositePageComponent,
-    data : { operation : 'load_nav' }/*,
-    canActivate: [AuthGuard]*/
+    data : { operation : 'load_nav' },
+    canActivate: [AuthGuard]
   },
   { path: 'application/design/create/object/:objectTypeId/:objectId',
     component: CompositePageComponent,
-    data : { operation : 'load_form' }/*,
-    canActivate: [AuthGuard]*/
+    data : { operation : 'load_form' },
+    canActivate: [AuthGuard]
   },
   { path: 'application/api/load/details/:objectTypeId/:objectId',
     component: CompositePageComponent,
-    data : { operation : 'load_details' }/*,
-    canActivate: [AuthGuard]*/
+    data : { operation : 'load_details' },
+    canActivate: [AuthGuard]
   },
 
   // otherwise redirect to home
